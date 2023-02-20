f4ff1104c3e3f7589c2c270f53a4c6592a199e46
Amir McCutchen <3amproduced@gmail.com>
2023-02-20 15:17:22 -0500

    completed add/remove friend endpoint

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -34,8 +34,48 @@ export const getUserFriends = async (req, res) => {
     };
 };
 
+// adding and removing friends
+
 export const addRemoveFriend = async (req, res) => {
     try {
+        const { id, friendId } = req.params;
+        const user = await Users.findById(id);
+        const friend = await Users.findById(friendId);
+
+        if (user.friends.includes(friendId)) {
+
+            /* removing a friend
+
+            removing user that is already present on the users friends list and that user from the other user's list */
+
+            user.friends = user.friends.filter(id => id != friendId);
+            friend.friends = friend.friends.filter(id => id != id);
+
+        } else {
+
+            /* adding a friend
+
+            if they aren't already present, add them to the friends list of both users */
+
+            user.friends.push(friendId);
+            friend.friends.push(id);
+
+
+        };
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => Users.findById(id))
+        );
+        const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+            return { _id, firstName, lastName, occupation, location, picturePath };
+        }
+    );
+
+    res.status(200).json(formattedFriends);
 
     } catch (e) {
         res.status(404).json({ message: e.message });
