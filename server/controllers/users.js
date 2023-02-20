f28054b4c7b46ea2fb2df5645012ace1508ecaa9
Amir McCutchen <3amproduced@gmail.com>
2023-02-20 15:00:04 -0500

    completed getuserfriends endpoint

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,7 +3,7 @@ import Users from '../models/Users.js';
 export const getUser = async (req, res) => {
     try {
 
-        // finding user based on id
+        // sending user based on id
 
         const { id } = req.params;
         const user = await Users.findById(id);
@@ -16,9 +16,21 @@ export const getUser = async (req, res) => {
 
 export const getUserFriends = async (req, res) => {
     try {
+        const { id } = req.params;
+        const user = await Users.findById(id);
+
+        const friends = await Promise.all(
+            user.friends.map((id) => Users.findById(id))
+        );
+        const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+            return { _id, firstName, lastName, occupation, location, picturePath };
+        }
+    );
+    res.status(200).json(formattedFriends);
 
     } catch (e) {
-
+        res.status(404).json({ message: e.message });
     };
 };
 
@@ -26,6 +38,6 @@ export const addRemoveFriend = async (req, res) => {
     try {
 
     } catch (e) {
-
+        res.status(404).json({ message: e.message });
     };
 };
