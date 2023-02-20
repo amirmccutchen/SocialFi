e132325e477030217c123486dedaf6f618f860a7
Amir McCutchen <3amproduced@gmail.com>
2023-02-20 13:35:54 -0500

    completed login endpoint

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -61,7 +61,11 @@ export const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.'})
 
+        // sending user their auth token
+
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user});
 
     } catch (e) {
         res.status(500).json({ error: e.message });
