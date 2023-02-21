a961d9c40ba7d394b1fddd2302965187260820ae
Amir McCutchen <3amproduced@gmail.com>
2023-02-21 15:18:51 -0500

    created posts routes

---
+++ server/routes/posts.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { verify } from 'jsonwebtoken';
+import { getFeedPosts, getUserPosts, likePost} from '../controllers/posts.js';
+import { verifyToken } from '../middleware/auth.js';
+
+const router = express.Router();
+
+// viewing posts on feed
+
+router.get('/', verifyToken, getFeedPosts);
+
+// viewing user's posts
+
+router.get('/:userId/posts', verifyToken, getUserPosts);
+
+// liking and unliking posts
+
+router.patch('/:id/like', verifyToken, likePost);
