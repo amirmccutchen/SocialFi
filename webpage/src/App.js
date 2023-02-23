99aa3e4727d338c5b45f65bf54cbb7d178974ec9
Amir McCutchen <3amproduced@gmail.com>
2023-02-23 18:20:30 -0500

    imported theme to appjs

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { Home, Login, NavBar, Profile } from "pages";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+
+  const mode = useSelector(state => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className = "app">
       <BrowserRouter>
-        <Routes>
-          <Route path = '/'     element = {<Login />} />
-          <Route path = '/home' element = {<Home  />}  />
-          <Route path = '/profile/:userId' element ={<Profile />} />
-        </Routes>
+        <ThemeProvider theme = {theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path = '/'     element = {<Login />} />
+            <Route path = '/home' element = {<Home  />}  />
+            <Route path = '/profile/:userId' element ={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
