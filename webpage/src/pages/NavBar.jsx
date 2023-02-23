13ebe8cc22a9fad3d9c77112dd21c198c41bdbf6
Amir McCutchen <3amproduced@gmail.com>
2023-02-23 18:46:06 -0500

    navbar formatting

--- webpage/src/pages/NavBar.jsx
+++ webpage/src/pages/NavBar.jsx
@@ -55,12 +55,12 @@ const NavBar = () => {
   const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
-    <FlexBetween padding='1rem 6%' backgroundColor={alt}>
-      <FlexBetween gap='1.75rem'>
+    <FlexBetween padding = '1rem 6%' backgroundColor={alt}>
+      <FlexBetween gap = '1.75rem'>
         <Typography
-          fontWeight='bold'
-          fontSize='clamp(1rem, 2rem, 2.25rem)'
-          color='primary'
+          fontWeight = 'bold'
+          fontSize = 'clamp(1rem, 2rem, 2.25rem)'
+          color = 'primary'
           onClick={() => navigate('/home')}
           sx={{
             '&:hover': {
@@ -69,14 +69,14 @@ const NavBar = () => {
             },
           }}
         >
-          Sociopedia
+          SocialFi
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
-            backgroundColor={neutralLight}
-            borderRadius='9px'
-            gap='3rem'
-            padding='0.1rem 1.5rem'
+            backgroundColor = {neutralLight}
+            borderRadius = '9px'
+            gap = '3rem'
+            padding = '0.1rem 1.5rem'
           >
             <InputBase placeholder='Search...' />
             <IconButton>
@@ -88,21 +88,21 @@ const NavBar = () => {
 
       {/* desktop */}
       {isNonMobileScreens ? (
-        <FlexBetween gap='2rem'>
-          <IconButton onClick={() => dispatch(setMode())}>
+        <FlexBetween gap = '2rem'>
+          <IconButton onClick = {() => dispatch(setMode())}>
             {theme.palette.mode === 'dark' ? (
-              <DarkMode sx={{ fontSize: '25px' }} />
+              <DarkMode sx = {{ fontSize: '25px' }} />
             ) : (
-              <LightMode sx={{ color: dark, fontSize: '25px' }} />
+              <LightMode sx = {{ color: dark, fontSize: '25px' }} />
             )}
           </IconButton>
-          <Message sx={{ fontSize: '25px' }} />
-          <Notifications sx={{ fontSize: '25px' }} />
-          <Help sx={{ fontSize: '25px' }} />
-          <FormControl variant='standard' value={fullName}>
+          <Message sx = {{ fontSize: '25px' }} />
+          <Notifications sx = {{ fontSize: '25px' }} />
+          <Help sx = {{ fontSize: '25px' }} />
+          <FormControl variant = 'standard' value = {fullName}>
             <Select
-              value={fullName}
-              sx={{
+              value = {fullName}
+              sx = {{
                 backgroundColor: neutralLight,
                 width: '150px',
                 borderRadius: '0.25rem',
@@ -115,9 +115,9 @@ const NavBar = () => {
                   backgroundColor: neutralLight,
                 },
               }}
-              input={<InputBase />}
+              input = {<InputBase />}
             >
-              <MenuItem value={fullName}>
+              <MenuItem value = {fullName}>
                 <Typography>{fullName}</Typography>
               </MenuItem>
               <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
@@ -135,19 +135,19 @@ const NavBar = () => {
       {/* mobile */}
       {!isNonMobileScreens && isMobileMenuToggled && (
         <Box
-          position='fixed'
-          right='0'
-          bottom='0'
-          height='100%'
-          zIndex='10'
-          maxWidth='500px'
-          minWidth='300px'
-          backgroundColor={background}
+          position = 'fixed'
+          right = '0'
+          bottom = '0'
+          height = '100%'
+          zIndex = '10'
+          maxWidth = '500px'
+          minWidth = '300px'
+          backgroundColor = {background}
         >
           {/* close icon */}
-          <Box display='flex' justifyContent='flex-end' p='1rem'>
+          <Box display = 'flex' justifyContent = 'flex-end' p = '1rem'>
             <IconButton
-              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+              onClick = {() => setIsMobileMenuToggled(!isMobileMenuToggled)}
             >
               <Close />
             </IconButton>
@@ -155,29 +155,29 @@ const NavBar = () => {
 
           {/* menu items */}
           <FlexBetween
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            gap='3rem'
+            display = 'flex'
+            flexDirection = 'column'
+            justifyContent = 'center'
+            alignItems = 'center'
+            gap = '3rem'
           >
             <IconButton
-              onClick={() => dispatch(setMode())}
-              sx={{ fontSize: '25px' }}
+              onClick = {() => dispatch(setMode())}
+              sx = {{ fontSize: '25px' }}
             >
               {theme.palette.mode === 'dark' ? (
-                <DarkMode sx={{ fontSize: '25px' }} />
+                <DarkMode sx = {{ fontSize: '25px' }} />
               ) : (
-                <LightMode sx={{ color: dark, fontSize: '25px' }} />
+                <LightMode sx = {{ color: dark, fontSize: '25px' }} />
               )}
             </IconButton>
-            <Message sx={{ fontSize: '25px' }} />
-            <Notifications sx={{ fontSize: '25px' }} />
-            <Help sx={{ fontSize: '25px' }} />
-            <FormControl variant='standard' value={fullName}>
+            <Message sx = {{ fontSize: '25px' }} />
+            <Notifications sx = {{ fontSize: '25px' }} />
+            <Help sx = {{ fontSize: '25px' }} />
+            <FormControl variant = 'standard' value = {fullName}>
               <Select
-                value={fullName}
-                sx={{
+                value = {fullName}
+                sx = {{
                   backgroundColor: neutralLight,
                   width: '150px',
                   borderRadius: '0.25rem',
@@ -190,12 +190,12 @@ const NavBar = () => {
                     backgroundColor: neutralLight,
                   },
                 }}
-                input={<InputBase />}
+                input = {<InputBase />}
               >
-                <MenuItem value={fullName}>
+                <MenuItem value = {fullName}>
                   <Typography>{fullName}</Typography>
                 </MenuItem>
-                <MenuItem onClick={() => dispatch(setLogout())}>
+                <MenuItem onClick = {() => dispatch(setLogout())}>
                   Log Out
                 </MenuItem>
               </Select>
