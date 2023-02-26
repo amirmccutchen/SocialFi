f6044355339f61ebd6ca317f6aefef088ee1e83a
Amir McCutchen <3amproduced@gmail.com>
2023-02-26 16:27:58 -0500

    added unauthorized conditionals to app js

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,26 +1,34 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import { Home, Login, NavBar, Profile } from "pages";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { Home, Login, Profile } from 'pages';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 function App() {
 
-  const mode = useSelector(state => state.mode);
+  const mode = useSelector((state) => state.mode);
+
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <div className = "app">
+    <div className = 'app'>
       <BrowserRouter>
         <ThemeProvider theme = {theme}>
           <CssBaseline />
+          
+          {/* if the user is not authorized, send them to the 'login' home page */}
+
           <Routes>
-            <Route path = '/'     element = {<Login />} />
-            <Route path = '/home' element = {<Home  />}  />
-            <Route path = '/profile/:userId' element ={<Profile />} />
+
+            <Route path = '/'                element = {<Login />}  />
+            <Route path = '/home'            element = {isAuth ? <Home    /> : <Navigate to = '/' />} />
+            <Route path = '/profile/:userId' element = {isAuth ? <Profile /> : <Navigate to = '/' />} />
+
           </Routes>
+
         </ThemeProvider>
       </BrowserRouter>
     </div>
